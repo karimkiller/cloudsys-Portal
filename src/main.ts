9e1eb25cb01b8699557c54efcb729fe253a556d5
karimkiller <karimadel66.ka@gmail.com>
2025-11-11 16:45:12 -0500

    fix(swagger): use ExpressAdapter + return express handler; add /docs redirect

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,35 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import * as express from 'express'
 
-let server: any
+const server = express()
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+async function createServer() {
+  // Create Nest on top of a shared Express instance
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
   app.enableCors()
 
-  // Swagger at /docs
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('CloudSys API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('docs', app, document)
+  SwaggerModule.setup('/docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+    customSiteTitle: 'CloudSys API Docs',
+  })
 
-  // IMPORTANT: on Vercel DO NOT listen; just init and return the Express handler
+  // Helpful redirect: /docs -> /docs/
+  server.get('/docs', (_req, res) => res.redirect('/docs/'))
+
+  // IMPORTANT for serverless: no app.listen(); just init and return the Express handler
   await app.init()
-  return app.getHttpAdapter().getInstance()
+  return server
 }
 
-// @vercel/nestjs expects the default export to be a request handler
-export default (async () => (server ??= await bootstrap()))()
+// Vercel expects default export to be the request handler
+export default createServer()
