d35b0b823e53210c8e2876ffb9f5647414c53c34
karimkiller <karimadel66.ka@gmail.com>
2025-11-11 15:57:07 -0500

    fix: clean backend build (no Angular), correct imports, remove serverless entry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { PrismaService } from './prisma/prisma.service'
+import { PrismaService } from './common/prisma/prisma.service'
 import { AuthModule } from './auth/auth.module'
 import { TicketsModule } from './tickets/tickets.module'
 import { TimeEntriesModule } from './time-entries/time-entries.module'
-// TODO: import TemplatesModule, FormsModule, ClientsModule, ProjectsModule, AnnouncementsModule
-
 
 @Module({
-imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TicketsModule, TimeEntriesModule],
-providers: [PrismaService],
+  imports: [AuthModule, TicketsModule, TimeEntriesModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
